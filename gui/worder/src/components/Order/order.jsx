32d10889e10db474fe641264bc480dc5c0443cda
kazumakato <k.kato@walls-inc.com>
2022-06-23 22:24:12 +0900

    各種ボタン追加

--- gui/worder/src/components/Order/order.jsx
+++ gui/worder/src/components/Order/order.jsx
@@ -3,6 +3,9 @@ import { useState, useEffect } from "react";
 import Header from '../Common/header';
 import Button from '@mui/material/Button';
 import axios from "axios";
+import DeleteIcon from '@mui/icons-material/Delete';
+//import CheckIcon from '@mui/icons-material/Check';
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
 
 // MUIDataTableを利用する
 import MUIDataTable from "mui-datatables";
@@ -15,8 +18,23 @@ function About() {
   const [columns, setColumns] = useState()
   useEffect(() => {
     setColumns([{label: 'テーブル', name:'seat'},
-                {label: 'メニュー', name:'menu'},                
-                {label: '注文時間', name:'created_at'}])    
+                {label: 'メニュー', name:'menu'},             
+                {label: '注文時間', name:'created_at'},
+                {label: '提供', name:'order_st',options: {
+                  customBodyRender: () => {
+                    return (
+                      <Button variant="solid" onClick={GetOrder}><ShoppingCartCheckoutIcon/></Button>
+                    );
+                  }
+                }},
+                {label: 'キャンセル', name:'order_st',options: {
+                  customBodyRender: () => {
+                    return (
+                      <Button variant="solid" onClick={GetOrder}><DeleteIcon/></Button>
+                    );
+                  }
+                }}
+              ]) 
     GetOrder()
   },[])
 
@@ -27,8 +45,19 @@ function About() {
       console.log(res.data)
 
     })
-
   }
+
+  const options = {
+    selectableRowsHeader: false,
+    selectableRowsHideCheckboxes:true,
+    search:false,
+    download:false,
+    print:false,
+    viewColumns:false,
+    filter:false, 
+    sort: false
+  }
+
   return (
   <>
   <Header />
@@ -36,6 +65,7 @@ function About() {
         title=""
         data={data}
         columns={columns}
+        options={options}
       />
   <div><Button variant="solid" onClick={GetOrder}>更新</Button></div>
   </>
