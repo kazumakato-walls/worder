55b7f70e20bba9ff4498c4d2a9a6417157fcdcb2
junsato-walls <j.sato@walls-inc.com>
2022-06-28 14:52:56 +0900

    menu画面作成中

--- gui/worder/src/components/Menu/MenuCard.jsx
+++ gui/worder/src/components/Menu/MenuCard.jsx
@@ -1,33 +1,55 @@
 import * as React from 'react';
+// import { useState, useEffect } from "react";
 
+//グリッドで分けている部分
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+// import MenuCard from './MenuCard';
+
+//カード部分
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-import image from './curry.jpg'
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
-function MenuCard() {
+function menu() {
+ const img = require('./curry.jpg')
+    
+  return (
     <>
-        <Card sx={{ maxWidth: 500 }}>
-            <CardActionArea>
-                <CardMedia
+       <Grid item xs={6} sm={6} md={3} lg={3}>
+            <Item>
+              <Card sx={{ maxWidth: 500 }}>
+                <CardActionArea>
+                  <CardMedia
                     component="img"
                     height="140"
-                    image={image}
-                />
-                <CardContent>
+                    image={img}
+                  />
+                  <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
-                        カレー
+                      カレー
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                        説明文：テストテストテスト
+                      説明文：テストテストテスト
                     </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Item>
+        </Grid>
     </>
+  )
 }
-
-export default MenuCard
+export default menu;
